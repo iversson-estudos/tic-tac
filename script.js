d9aa9c7e90496d4dcdca25670d45ffe875499cf5
iversson-estudos <iversson.estudos@gmail.com>
2024-07-29 10:08:06 -0300

    [P] Salvando progresso2

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 //Store gameboard as an array inside of a gameboard object
-//GAMERBOARD OBJECTO
+//GAMERBOARD OBJECT
 
 function  createGameboard (){
+    
+    //creates gameboard
     const gameboardCells = new Array(9).fill('');
     
-} 
+    
+    
+
+
+}
+     
