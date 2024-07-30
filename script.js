c0a709a096dc93c6d9cc24e0465f3c2158c7f4c5
Iversson.Estudos <iversson.estudos@gmail.com>
2024-07-29 21:05:16 -0300

    started working on the logic behind the encapsulation and modules

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 //Store gameboard as an array inside of a gameboard object
 //GAMERBOARD OBJECTO
 
-function  createGameboard (){
-    const gameboardCells = new Array(9).fill('');
-    
-} 
+function tttGame() {
+  function createGameboard() {
+    const gameboardCells = new Array(9).fill("");
+
+    function playRound(marker, place) {
+      gameboardCells[place] = marker;
+    }
+
+    function checkWin() {}
+
+    return gameboardCells;
+  }
+}
