469dfca33050e1cdf3c8d21e7debbbdd48b953e4
iversson-estudos <iversson.estudos@gmail.com>
2024-08-01 10:09:18 -0300

    [P] Finished the main functions

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
 //Store gameboard as an array inside of a gameboard object
 
 
-function tttGame() {
+
   
   
   function createGameboard() {
     const gameboardCells = new Array(9).fill("");
-    let turnToPlay = 'X';
 
     function playRound(marker, place) {
-      gameboardCells[place] = marker;
-      
+      gameboardCells[place-1] = marker;      
     }
 
     function checkWin() {
       const possibleWins = [[1,2,3],[1,4,7],[1,5,9],[2,5,8],[3,5,7],[3,6,9],[4,5,8],[7,8,9]];
       
-      possibleWins.forEach((element) => {
-        if (gameboardCells[element[0]]===''|| gameboardCells[element[0]]=== null)
+     for (const element of possibleWins) {
+        if (gameboardCells[element[0]-1]===''|| gameboardCells[element[0]-1]=== null)
           {
             return false;
           }
 
-        if(gameboardCells[element[0]]===gameboardCells[element[1]]&&
-           gameboardCells[element[1]]===gameboardCells[element[2]])
+        if(gameboardCells[element[0]-1]===gameboardCells[element[1]-1]&&
+           gameboardCells[element[1]-1]===gameboardCells[element[2]-1])
            {
             return true;
            }
+        };
+        
+        return false;
+      }
 
-
-      
-      
-      });
-
-
-
-    }
-
-    return gameboardCells;
+    return {gameboardCells, checkWin, playRound};
   }
-}
+ 
