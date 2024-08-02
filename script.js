23cefc99420ea06a0cbae248e44d803a257f6a65
iversson-estudos <iversson.estudos@gmail.com>
2024-08-02 08:32:20 -0300

    [P]Added logic to reset the board and some minor changes aswell

--- script.js
+++ script.js
@@ -6,18 +6,40 @@
   
   function createGameboard() {
     const gameboardCells = new Array(9).fill("");
+    let marker='X';
 
-    function playRound(marker, place) {
-      gameboardCells[place-1] = marker;      
+
+    //PUTS THE MARKER WHEN PLAYER PLAYS
+    function playRound(place) {
+      if(gameboardCells[place-1]==='' )
+        {
+          gameboardCells[place-1] = marker;      
+        }
+    }
+    //ALTERNATES BETWEEN ROUND
+    function changePlayer(){
+      if(marker==='X'){marker='O';}
+      else {marker='X';}
+    }
+    //RETURNS GAMEBOARD
+    function getGameboard(){
+      return gameboardCells;
     }
 
+    //RESETS BOARD
+    function resetBoard (){
+    gameboardCells.fill("");
+    marker='X';    
+    }
+    
+    //CHECKS IF CURRENT PLAYER WON
     function checkWin() {
       const possibleWins = [[1,2,3],[1,4,7],[1,5,9],[2,5,8],[3,5,7],[3,6,9],[4,5,8],[7,8,9]];
       
      for (const element of possibleWins) {
         if (gameboardCells[element[0]-1]===''|| gameboardCells[element[0]-1]=== null)
           {
-            return false;
+            continue;
           }
 
         if(gameboardCells[element[0]-1]===gameboardCells[element[1]-1]&&
@@ -30,6 +52,6 @@
         return false;
       }
 
-    return {gameboardCells, checkWin, playRound};
+    return {changePlayer,getGameboard, checkWin, playRound,resetBoard};
   }
  
