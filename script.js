6a342b803028d2b99bbeaead7bda00656f39b569
iversson-estudos <iversson.estudos@gmail.com>
2024-09-17 06:28:16 -0300

    win check with wrong number

--- script.js
+++ script.js
@@ -276,7 +276,7 @@ const gameboard = (function () {
   
   //CHECKS IF CURRENT PLAYER WON
   function checkWin() {
-    const possibleWins = [[1,2,3],[1,4,7],[1,5,9],[2,5,8],[3,5,7],[3,6,9],[4,5,8],[7,8,9]];
+    const possibleWins = [[1,2,3],[1,4,7],[1,5,9],[2,5,8],[3,5,7],[3,6,9],[4,5,6],[7,8,9]];
     
    for (const element of possibleWins) {
       if (gameboardCells[element[0]-1]===''|| gameboardCells[element[0]-1]=== null)
